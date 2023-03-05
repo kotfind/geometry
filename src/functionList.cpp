1f0ec38609c73a4909d86902d97cb345fb0a88be
kotfind <kotfind@yandex.ru>
2023-03-05 14:47:33 +0300

    FunctionList macros

--- src/functionList.cpp
+++ src/functionList.cpp
@@ -4,10 +4,27 @@
 #include "Point.h"
 #include "Circle.h"
 
-Function f{
+#define CAT2(a,b) a##b // actually concatenate
+#define CAT(a,b) CAT2(a,b) // force expand
+#define FUNC Function CAT(function,__COUNTER__)
+
+#define DO [](const QList<Object*>& objs) -> QList<Object*>
+
+FUNC {
     "lineByTwoPoints",
     {Point::type, Point::type},
-    [](const QList<Object*>& objs) {
+    DO {
+        // XXX it's not working
+        auto* a = dynamic_cast<Point*>(objs[0]);
+        auto* b = dynamic_cast<Point*>(objs[1]);
+        return {new Line()};
+    }
+};
+
+FUNC {
+    "lineByTwoPoints2",
+    {Point::type, Point::type},
+    DO {
         // XXX it's not working
         auto* a = dynamic_cast<Point*>(objs[0]);
         auto* b = dynamic_cast<Point*>(objs[1]);
