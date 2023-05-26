1f43830aa985baa58b58752dfeb3fd1cffd4eec2
kotfind <kotfind@yandex.ru>
2023-05-26 12:50:42 +0300

    Clean up

--- src/hyperbolic/Transformation.cpp
+++ src/hyperbolic/Transformation.cpp
@@ -12,56 +12,19 @@ using namespace std::complex_literals;
 
 namespace hyperbolic::impl {
     void Transformation::scroll(const QPointF& delta) {
-        // XXX: remeber about metrix
-        z0 += std::complex<double>(delta.x(), delta.y()) * scrollSpeed;
+        // FIXME
     }
 
     void Transformation::move(const AbstractPoint* from, const AbstractPoint* to) {
-        // Solving equation:
-        // q = exp(1i * phi) * (z - z0) / (1 - conj(z0) * z)
-        //   where
-        //     z0 is variable,
-        //     z is from,
-        //     q is transformed(to).
-        //
-        // Getting equation:
-        // z0 - q * exp(-1i * phi) * z * conj(z0) = z - q * exp(-1i * phi)
-        //
-        // Let m = z - q * exp(-i * phi),
-        //     n = q * exp(-i * phi) * z,
-        //
-        // Getting equation:
-        // z - n * conj(z0) = m
-        //
-        // Getting equation system (in Re(z0) ans Im(z0))
-
-        auto z = static_cast<const Point*>(from)->getComplex();
-        auto to_ = std::unique_ptr<Point>(
-            static_cast<Point*>(to->clone())
-        );
-        transform(to_.get());
-        auto q = to_->getComplex();
-
-        auto m = z - q * std::exp(-1i * phi);
-        auto n = q * std::exp(-1i * phi) * z;
-
-        auto cramerAns = cramer({
-            //     a                   b               free
-            {1 - std::real(n),  -std::imag(n),      std::real(m)},
-            {-std::imag(n),     1 + std::real(n),   std::imag(m)},
-        });
-        assert(!cramerAns.isEmpty());
-
-        z0 = std::complex<double>(cramerAns[0], cramerAns[1]);
+        // FIXME
     }
 
     void Transformation::zoom(double v, const QPointF& zoomCenter) {
-        phi += v * rotationSpeed;
+        // FIXME
     }
 
     void Transformation::clear() {
-        phi = 0;
-        z0 = 0;
+        // FIXME
     }
 
     QJsonObject Transformation::toJson() const {
@@ -73,19 +36,10 @@ namespace hyperbolic::impl {
     }
 
     void Transformation::transform(AbstractPoint* pt) const {
-        auto& p = *static_cast<Point*>(pt);
-        auto z = p.getComplex();
-        p.setComplex(
-            std::exp(phi * 1i) * (z - z0) / (1. - std::conj(z0) * z)
-        );
+        // FIXME
     }
 
     void Transformation::untransform(AbstractPoint* pt) const {
-        auto& p = *static_cast<Point*>(pt);
-        auto e = std::exp(-1i * phi);
-        auto z = p.getComplex();
-        p.setComplex(
-            (z * e + z0) / (z * std::conj(z0) * e + 1.)
-        );
+        // FIXME
     }
 }
