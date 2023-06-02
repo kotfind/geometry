66540f26451423261b2d9f3c7be17a7b60dc1f0f
kotfind <kotfind@yandex.ru>
2023-06-02 22:53:01 +0300

    Create EditMode icons

--- src/core/EditMode.cpp
+++ src/core/EditMode.cpp
@@ -42,7 +42,7 @@ EditMode* EditMode::get(Type t) {
 EditMode moveMode(
     EditMode::Type::MOVE,
     TR("Move"),
-    QIcon(QIcon(":none.svg")),
+    QIcon(QIcon(":move.svg")),
     TR(
         "Drag any free object with mouse to move it. "
         "Use middle mouse button to move around scene."
@@ -52,14 +52,14 @@ EditMode moveMode(
 EditMode createPointMode(
     EditMode::Type::CREATE_POINT,
     TR("Create"),
-    QIcon(QIcon(":none.svg")),
+    QIcon(QIcon(":create_point.svg")),
     TR("Click on the canvas to create a point.")
 );
 
 EditMode removeMode(
     EditMode::Type::REMOVE,
     TR("Remove"),
-    QIcon(QIcon(":none.svg")),
+    QIcon(QIcon(":remove.svg")),
     TR("Click on any object to delete it.")
 );
 
@@ -73,6 +73,6 @@ EditMode functionEditMode(
 EditMode hideMode(
     EditMode::Type::HIDE,
     TR("Hide/Show"),
-    QIcon(QIcon(":none.svg")),
+    QIcon(QIcon(":hide.svg")),
     TR("Click on any object to toggle it's visibility.")
 );
