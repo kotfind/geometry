9796f6dc3ef7f8c22b7bd51bd25c5f90f0c34fff
kotfind <kotfind@yandex.ru>
2023-06-22 14:02:43 +0300

    Remove run.cpp/.h

--- src/core/run.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "AbstractGeometry.h"
-
-#include <memory>
-#include <QList>
-
-int run(int argc, char** argv, const QList<AbstractGeometry*>& geoms);
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,26 @@
-#include "core/run.h"
+#include "core/MainWindow.h"
+#include "core/Engine.h"
 
 #include "euclidian/Geometry.h"
 #include "hyperbolic/Geometry.h"
 
+#include <QApplication>
+
+class AbstractGeometry;
+
 int main(int argc, char** argv) {
+    QApplication app(argc, argv);
+
+    QCoreApplication::setOrganizationName("kotfindApps");
+    QCoreApplication::setApplicationName("Geometry");
+
     euclidian::Geometry eGeom;
     hyperbolic::Geometry hGeom;
 
-    return run(argc, argv, { &eGeom, &hGeom });
+    Engine engine({ &eGeom, &hGeom });
+
+    MainWindow master(&engine);
+    master.show();
+
+    return app.exec();
 }
